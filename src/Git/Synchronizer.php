9d36119ba8d32bae8211b30280e6c75781621e6e
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-08-19 09:48:40 +0200

    Initial version of synchronizer
    This implements a console command which given a file containing a
    GitHub event in JSON format will push the changes to a target
    repository.
    
    The implementation is based on a few key packages:
    
    - GitHubEventParser for extracting data from GitHub events
    - GitWrapper for working with Git

---
+++ src/Git/Synchronizer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Git;
+
+use GitWrapper\GitWrapper;
+
+class Synchronizer
+{
+
+  /* @var \GitWrapper\GitWrapper */
+  private $git;
+
+  /* @var string */
+  private $workingDirectory;
+
+  /* @var string */
+  private $targetRepoUrl;
+
+  public function __construct(GitWrapper $git, string $workingDirectory, string $targetRepoUrl)
+  {
+    $this->git = $git;
+    $this->workingDirectory = $workingDirectory;
+    $this->targetRepoUrl = $targetRepoUrl;
+  }
+
+  public function synchronizeBranch(string $repoUrl, string $branch)
+  {
+    $targetDirectory = $this->workingDirectory . '/' . uniqid();
+    $repository = $this->git->cloneRepository($repoUrl, $targetDirectory);
+    $repository->checkout($branch);
+
+    $repository->addRemote('target', $this->targetRepoUrl);
+
+    $repository->push('--force', 'target', $branch);
+  }
+
+}
