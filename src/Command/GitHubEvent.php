96c8a907e8eef25780b6607fa136e82147842fee
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-08-19 10:03:57 +0200

    Update code style to match PSR12
    Add PHPCodesniffer as a dependency and make it check the code for
    PSR12 compliance.
    
    Add .editorconfig to try to make editors follow some PSR12 standards
    by default.

--- src/Command/GitHubEvent.php
+++ src/Command/GitHubEvent.php
@@ -8,47 +8,49 @@ use App\Git\Synchronizer;
 use Lpdigital\Github\EventType\PullRequestEvent;
 use Lpdigital\Github\Parser\WebhookResolver;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GitHubEvent extends Command
 {
 
-  // the name of the command (the part after "bin/console")
-  protected static $defaultName = 'app:github-event';
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:github-event';
 
-  /* @var \Lpdigital\Github\Parser\WebhookResolver */
-  private $resolver;
+    /* @var \Lpdigital\Github\Parser\WebhookResolver */
+    private $resolver;
 
-  /* @var \App\Git\Synchronizer */
-  private $synchronizer;
+    /* @var \App\Git\Synchronizer */
+    private $synchronizer;
 
-  /* @var string */
-  private $eventData;
+    /* @var string */
+    private $eventData;
 
-  public function __construct(WebhookResolver $resolver, Synchronizer $synchronizer, array $eventData)
-  {
-    $this->resolver = $resolver;
-    $this->synchronizer = $synchronizer;
-    $this->eventData = $eventData;
+    public function __construct(
+        WebhookResolver $resolver,
+        Synchronizer $synchronizer,
+        array $eventData
+    ) {
+        $this->resolver = $resolver;
+        $this->synchronizer = $synchronizer;
+        $this->eventData = $eventData;
 
-    parent::__construct();
-  }
-
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    /* @var \Lpdigital\Github\EventType\GithubEventInterface $event */
-    $event = $this->resolver->resolve($this->eventData);
-    if (!$event instanceof PullRequestEvent) {
-      throw new \UnexpectedValueException('Unsupported event type: ' . $event::name());
+        parent::__construct();
     }
 
-    $head = $event->pullRequest->getHead();
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /* @var \Lpdigital\Github\EventType\GithubEventInterface $event */
+        $event = $this->resolver->resolve($this->eventData);
+        if (!$event instanceof PullRequestEvent) {
+            throw new \UnexpectedValueException('Unsupported event type: ' . $event::name());
+        }
 
-    $this->synchronizer->synchronizeBranch(
-      $head['repo']['git_url'],
-      $head['ref']);
-  }
+        $head = $event->pullRequest->getHead();
 
+        $this->synchronizer->synchronizeBranch(
+            $head['repo']['git_url'],
+            $head['ref']
+        );
+    }
 }
--- src/Git/Synchronizer.php
+++ src/Git/Synchronizer.php
@@ -9,31 +9,30 @@ use GitWrapper\GitWrapper;
 class Synchronizer
 {
 
-  /* @var \GitWrapper\GitWrapper */
-  private $git;
+    /* @var \GitWrapper\GitWrapper */
+    private $git;
 
-  /* @var string */
-  private $workingDirectory;
+    /* @var string */
+    private $workingDirectory;
 
-  /* @var string */
-  private $targetRepoUrl;
+    /* @var string */
+    private $targetRepoUrl;
 
-  public function __construct(GitWrapper $git, string $workingDirectory, string $targetRepoUrl)
-  {
-    $this->git = $git;
-    $this->workingDirectory = $workingDirectory;
-    $this->targetRepoUrl = $targetRepoUrl;
-  }
+    public function __construct(GitWrapper $git, string $workingDirectory, string $targetRepoUrl)
+    {
+        $this->git = $git;
+        $this->workingDirectory = $workingDirectory;
+        $this->targetRepoUrl = $targetRepoUrl;
+    }
 
-  public function synchronizeBranch(string $repoUrl, string $branch)
-  {
-    $targetDirectory = $this->workingDirectory . '/' . uniqid();
-    $repository = $this->git->cloneRepository($repoUrl, $targetDirectory);
-    $repository->checkout($branch);
+    public function synchronizeBranch(string $repoUrl, string $branch)
+    {
+        $targetDirectory = $this->workingDirectory . '/' . uniqid();
+        $repository = $this->git->cloneRepository($repoUrl, $targetDirectory);
+        $repository->checkout($branch);
 
-    $repository->addRemote('target', $this->targetRepoUrl);
+        $repository->addRemote('target', $this->targetRepoUrl);
 
-    $repository->push('--force', 'target', $branch);
-  }
-
+        $repository->push('--force', 'target', $branch);
+    }
 }
