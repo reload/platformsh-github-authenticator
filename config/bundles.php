e009b2d23b4b33c98add73c9c7ec832656e59755
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-09-16 15:38:40 +0200

    Migrate to Enqueue Messenger Transport
    This adds support for different queue providers including Amazon SNS
    and SQS.
    
    For development purposes we use the filesystem as a queue. This way
    assessing the state of queue becomes very accessible.

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Swop\Bundle\GitHubWebHookBundle\GitHubWebHookBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
 ];
