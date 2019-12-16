33fab16d0e4f2f005470ef2ae6eb67e105e40a64
Kasper Garnaes <kasper.garnaes@gmail.com>
2019-12-16 12:33:16 +0100

    Log nested handler failed exceptions separately
    Otherwise we cannot tell what caused the problem.

--- src/EventSubscriber/SnsErrorLogger.php
+++ src/EventSubscriber/SnsErrorLogger.php
@@ -7,6 +7,8 @@ use Bref\MessengerSns\Event\SnsMessageFailed;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 
 class SnsErrorLogger implements EventSubscriberInterface, LoggerAwareInterface
 {
@@ -29,12 +31,23 @@ class SnsErrorLogger implements EventSubscriberInterface, LoggerAwareInterface
      */
     public function logError($error)
     {
-        $this->logger->error(
-            'SNS error',
-            [
-                'error' => get_class($error),
-                'exception' => get_class($error->getThrowable())
-            ]
-        );
+        $throwables = [ $error->getThrowable() ];
+
+        $throwable = $error->getThrowable();
+        if ($throwable instanceof HandlerFailedException) {
+            $throwables = array_merge($throwables, $throwable->getNestedExceptions());
+        }
+
+        array_map(function (\Throwable $throwable) use ($error) {
+            $this->logger->error(
+                'SNS error',
+                [
+                    'error' => get_class($error),
+                    'exception_class' => get_class($throwable),
+                    'exception' => $throwable,
+                    'trace' => $throwable->getTrace()
+                ]
+            );
+        }, $throwables);
     }
 }
